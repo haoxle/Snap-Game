f70d558842577672d4e69cb662fcf9da07c65fcc
haoxle <hao-le@live.co.uk>
2022-11-23 15:30:39 +0000

    class Card creation updated

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,25 +1,28 @@
 package org.example;
 
 public class Card {
-    private String suit;
-    private String symbol;
-    private int value;
+    public static String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    public static String[] symbol = {
+            null, "Ace", "2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "Jack", "Queen", "King"};
+    public static int[] value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-    public Card(String suit, String symbol, int value) {
+
+    public Card(String[] suit, String[] symbol, int[] value) {
         this.suit = suit;
         this.symbol = symbol;
         this.value = value;
     }
 
-    public int getValue() {
+    public int[] getValue() {
         return value;
     }
 
-    public String getSymbol() {
+    public String[] getSymbol() {
         return symbol;
     }
 
-    public String getSuit() {
+    public String[] getSuit() {
         return suit;
     }
 
