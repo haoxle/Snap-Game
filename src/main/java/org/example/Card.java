068626a776ddb8902fd6d8884c9fce9cf3a9ddbc
haoxle <hao-le@live.co.uk>
2022-11-23 15:03:24 +0000

    class Card creation

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Card {
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
 }
